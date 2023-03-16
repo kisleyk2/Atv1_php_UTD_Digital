85ea77c8083753c57f77f18678e49062df72952e
kisleyk2 <127906136+kisleyk2@users.noreply.github.com>
2023-03-16 16:31:20 -0300

    Update qst_2.php

--- qst_2.php
+++ qst_2.php
@@ -15,10 +15,10 @@
                                 <input type="text" name="v1" class="form-control v1" id="v1">
                             </div>
                             <div class="col">   
-                                <input type="text" name="opc" class="form-control v1" id="v2">
+                                <input type="text" name="opc" class="form-control opc text-center" id="opc" maxlength="1">
                             </div>
                             <div class="col border-3">
-                                <input type="text" name="v2" class="form-control v2" id="v3">
+                                <input type="text" name="v2" class="form-control v2" id="v2">
                             </div>
                             <div class="col border-3">
                                 <button class="btn btn-dark form-control result">?</button>
@@ -48,33 +48,33 @@
                         <div class="col"><button class="btn btn-success bt9" value="9" onclick="insert('9')">9</button></div>
                         <div class="col"><button class="btn btn-success bt8" value="8" onclick="insert('8')">8</button></div>
                         <div class="col"><button class="btn btn-success bt7" value="7" onclick="insert('7')">7</button></div>
-                        <div class="col"><button class="btn btn-success bt/" value="/" onclick="insert('/')">/</button></div>
+                        <div class="col"><button class="btn btn-success bt/" value="/" onclick="calc('/')">/</button></div>
                     </div>
                     <div class="row border border-dark p-2">
                         <div class="col"><button class="btn btn-success bt6" value="6" onclick="insert('6')">6</button></div>
                         <div class="col"><button class="btn btn-success bt5" value="5" onclick="insert('5')">5</button></div>
                         <div class="col"><button class="btn btn-success bt4" value="4" onclick="insert('4')">4</button></div>
-                        <div class="col"><button class="btn btn-success bt*" value="*" onclick="insert('*')">*</button></div>
+                        <div class="col"><button class="btn btn-success bt*" value="*" onclick="calc('*')">*</button></div>
                     </div>
                     <div class="row border border-dark p-2">
                         <div class="col"><button class="btn btn-success bt3" value="3" onclick="insert('3')">3</button></div>
                         <div class="col"><button class="btn btn-success bt2" value="2" onclick="insert('2')">2</button></div>
                         <div class="col"><button class="btn btn-success bt1" value="1" onclick="insert('1')">1</button></div>
-                        <div class="col"><button class="btn btn-success bt-" value="-" onclick="insert('-')">-</button></div>
+                        <div class="col"><button class="btn btn-success bt-" value="-" onclick="calc('-')">-</button></div>
                     </div>
                     <div class="row  border border-dark p-2">
                         <div class="col"><button class="btn btn-success bt0" value="0" onclick="insert('0')">0</button></div>
                         <div class="col">
-                            <button class="btn btn-success btr" value="r" onclick="insert('sqrt')">
+                            <button class="btn btn-success btr" value="r" onclick="calc('sqrt')">
                                 <span class="iconify" data-icon="la:square-root-alt"></span>
                             </button>
                         </div>
                         <div class="col">
-                            <button class="btn btn-success btex" value="e" onclick="insert('^')">
+                            <button class="btn btn-success btex" value="e" onclick="calc('^')">
                                 <span class="iconify" data-icon="mdi:exponent"></span>
                             </button>
                         </div> 
-                        <div class="col"><button class="btn btn-success bt+" value="+" onclick="insert('+')">+</button></div>
+                        <div class="col"><button class="btn btn-success bt+" value="+" onclick="calc('+')">+</button></div>
                     </div>
                 </div>
             </div>
@@ -82,19 +82,17 @@
 
         <script>
             var v1 = document.querySelector("#v1");
+            var opc = document.querySelector("#opc");
             var v2 = document.querySelector("#v2");
-            var v3 = document.querySelector("#v3");
 
             var cod = '1';
             function getfocus(c) {
                 if (c == '1') 
                     v1.focus();                    
-                else if (c == '2')
+                else 
                     v2.focus();
-                else
-                    v3.focus();
                 
-                if (c < 3)
+                if (c < 2)
                     c = Number(c) + 1;
                 else
                     c = '1';
@@ -106,15 +104,18 @@
             function insert(dig) {
                 if (cod == '1')
                     v1.value += dig;
-                else if (cod == '2')
+                else 
                     v2.value += dig;
-                else {
-                    v3.value += dig;
-                }
+            }
 
-                if (v2.value == 'sqrt')
-                    v3.value = '0';
-                
+            function calc(dig) {
+                opc.value = dig;
+                v2.focus()
+                cod = '2';
+                document.getElementById('bt').value = String(cod);
+
+                if (opc.value == 'sqrt')
+                    v2.value = '0';
             }
             
             var inputText;
@@ -125,17 +126,14 @@
 
                 if (cod == '1') 
                     op = '1';
-                else if (cod == '2')
+                else 
                     op = '2';
-                else
-                    op = '3';
 
                 inputText = window.document.querySelector(`#v${op}`);
                 if(inputText.value.length){
                     inputText.value = inputText.value.substr(0, inputText.value.length -1);
                     inputText.focus();
-                }
-                                   
+                }                                   
             });           
         </script>
 
